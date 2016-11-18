73f7e71e5b76b9e986601af0ffc0bcbc8b641afc
Christian Törnqvist <toernqvist@gmail.com>
2016-11-18 13:20:14 +0100

    adding reference

--- calc_pi.erl
+++ calc_pi.erl
@@ -2,39 +2,40 @@
 -export([calc/2]).
 
 calc(N, Schedulers) ->
+    Ref = make_ref(),
     Pid = self(),
     [spawn_link(fun() ->
-			worker(round(N/Schedulers), Pid)
+			worker(round(N/Schedulers), Pid, Ref)
 		end)
      || _ <- lists:seq(1,Schedulers - 1)],
-    
-    worker(round(N/Schedulers) + N rem Schedulers, Pid),
-    receiveLoop(Schedulers, N).
+  
+    worker(round(N/Schedulers) + N rem Schedulers, Pid, Ref),
+    receiveLoop(Schedulers, N, Ref).
 
-receiveLoop(Schedulers, N) ->
-    receiveLoop(Schedulers, 0, N, 0).
-receiveLoop(0, Sum, N, _) ->
+receiveLoop(Schedulers, N, Ref) ->
+    receiveLoop(Schedulers, 0, N, 0, Ref).
+receiveLoop(0, Sum, N, _, _) ->
     (Sum/N)*4;
-receiveLoop(Schedulers, Sum, N, Pred) ->
+receiveLoop(Schedulers, Sum, N, Pred, Ref) ->
     receive
-	{success, NewSum} -> 
-	    receiveLoop(Schedulers - 1, Sum + NewSum, N, NewSum);
+	{success, Ref, NewSum} -> 
+	    receiveLoop(Schedulers - 1, Sum + NewSum, N, NewSum, Ref);
 	Error ->
 	    io:format("Unknown error: ~w~n", [Error]),
-	    receiveLoop(Schedulers - 1, Sum + Pred, N, Pred)
+	    receiveLoop(Schedulers - 1, Sum + Pred, N, Pred, Ref)
      end.
 
-worker(Points, Pid) ->
-    worker(Points, Pid, 0).
-worker(0, Pid, Sum) ->
-    Pid ! {success, Sum};
-worker(Points, Pid, Sum) ->
+worker(Points, Pid, Ref) ->
+    worker(Points, Pid, Ref, 0).
+worker(0, Pid, Sum, Ref) ->
+    Pid ! {success, Ref, Sum};
+worker(Points, Pid, Sum, Ref) ->
     case inUnitCircle() of
 	true ->
-	    worker(Points - 1, Pid, Sum + 1);
+	    worker(Points - 1, Pid, Sum + 1, Ref);
 	false ->
-	    worker(Points - 1, Pid, Sum)
+	    worker(Points - 1, Pid, Sum, Ref)
     end.
 
 inUnitCircle() ->
-    (math:pow((rand:uniform()),2) + math:pow((rand:uniform()),2)) < 1.
+    (math:pow((random:uniform()),2) + math:pow((random:uniform()),2)) < 1.
